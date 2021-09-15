ef4ad3721f183fc812f1e7536a4184678ed641ea
GHOUL_ANUP <mhranup@gmail.com>
2021-09-15 08:40:54 +0545

    Study of JS datatype

---
+++ index.js
@@ -0,0 +1,26 @@
+let a = 1;
+let b = "Hellow world";
+let c = true; 
+let d = 1.5;
+let f = null;
+a = "IIMS";
+//console.log (a);
+
+let person = {
+    name:"Anup",
+    age: 22,
+    address: "Kathmandu",
+    location: {
+        lng: 2.13,
+        lati: 1.2323,
+    }
+}
+//console.log("this persone name is",person.name);
+//console.log("the logitude of the location is", person.location.lati);
+
+
+let exampleArray = Array(1,2,3,4);
+ let exampleArray2= [1,2,3,4,5];
+  console.log(exampleArray);
+
+
