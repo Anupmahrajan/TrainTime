4e6f45c08d9baf5a2bc2fde57217f0270ac1f05d
GHOUL_ANUP <mhranup@gmail.com>
2021-09-20 08:10:54 +0545

    contional example addded

---
+++ conditional.js
@@ -0,0 +1,37 @@
+// const age = 25;
+// if (age <= 20) {
+//     console.log("you are in teen age")
+// } else if (age > 20 && age <= 29) {
+//     console.log ("your re in your twenty's");
+
+
+//     }else {
+//         console.log ("You are old");
+//     }
+
+
+    // let day = "Sunday";
+    // switch (day) {
+    //     case "monday":
+    //         console.log ("It is monday");
+    //         break;
+    //         case "tuesday" :
+    //             console.log ("it is tuesday");
+    //             break;
+    //         case "Sunday":
+    //             console.log ("it is SUNNNNYYYDAYYY");
+    //             break;
+    //             default:
+    //                 console.log("your week day is not available here");
+    // }
+
+    for(let i = 10; i >= 1; i--) {
+        if (i === 5) {
+            break;
+        }
+        if (i === 9){
+            continue;
+        }
+        console.log ("ANUP IS GREAT: ",i);
+
+    }
